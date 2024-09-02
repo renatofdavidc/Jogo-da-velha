4bc7ba11f92f750094a7432b89dde315786209b9
renatofdavidc <159155985+renatofdavidc@users.noreply.github.com>
2024-09-02 15:24:39 -0300

    Update jogodavelha.py

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,11 +1,26 @@
+'''
+FIAP
+1TDSA - Segundo semestre de 2024
+
+Checkpoint 4
+Arquivo: jogodavelha.py
+
+Renato de Freitas David Campiteli RM 555627
+Pedro Lucas de Oliveira Bezerra RM 558439
+Gabriel Santos Jablonski RM 555452
+
+04/09/2024
+'''
+
+#import do random, que é utilizado para escolher as posições aleatórias da máquina fácil
 import random
 
 #tabuleiro
 tabuleiro = [['', '', ''],
-          ['', '', ''],
-          ['', '', '']]
+             ['', '', ''],
+             ['', '', '']]
 
-#variaveis para guardar as pontuações
+#variáveis para guardar as pontuações
 pontuacao_jogador_x = 0
 pontuacao_jogador_o = 0
 
@@ -27,10 +42,10 @@ def inicializar_tabuleiro():
 
 #imprimir tabuleiro
 def imprimir_tabuleiro():
-    print(f'{tabuleiro[0][0]} | {tabuleiro[0][1]} | {tabuleiro[0][2]}')
-    print(f'{tabuleiro[1][0]} | {tabuleiro[1][1]} | {tabuleiro[1][2]}')
-    print(f'{tabuleiro[2][0]} | {tabuleiro[2][1]} | {tabuleiro[2][2]}')
-    
+    for linha in tabuleiro:
+        print(' | '.join([x if x else ' ' for x in linha]))
+        print('-' * 10)
+
 #receber coordenada da linha
 def coordenada_linha():
     return int(input('Escolha a linha para jogar (0, 1, 2): '))
@@ -49,8 +64,7 @@ def mostrar_pontuacao():
 
 #verificar se a coordenada escolhida é permitida
 def posicao_valida(linha, coluna):
-    return 0 <= linha < 3 and 0 <= coluna <3 and tabuleiro[linha][coluna] == ''
-
+    return 0 <= linha < 3 and 0 <= coluna < 3 and tabuleiro[linha][coluna] == ''
 
 #realizar jogada
 def jogar(jogador, linha, coluna):
@@ -58,18 +72,21 @@ def jogar(jogador, linha, coluna):
 
 #jogada do usuário
 def jogada_usuario(jogador):
-    linha = coordenada_linha()
-    coluna = coordenada_coluna()
-    if posicao_valida(linha, coluna):
-        jogar(jogador, linha, coluna)
-    else: print('Jogada inválida! Por favor, escolha outra.')
+    while True:
+        linha = coordenada_linha()
+        coluna = coordenada_coluna()
+        if posicao_valida(linha, coluna):
+            jogar(jogador, linha, coluna)
+            break
+        else:
+            print('Jogada inválida! Por favor, escolha outra.')
 
 #verificar se há uma sequência de 3 x ou o em qualquer direção
 def verificar_vencedor():
     for i in range(3):
-        if tabuleiro [i][0] == tabuleiro[i][1] == tabuleiro[i][2] != '':
+        if tabuleiro[i][0] == tabuleiro[i][1] == tabuleiro[i][2] != '':
             return tabuleiro[i][0]
-        if tabuleiro[0][i] == tabuleiro[1][i] == tabuleiro [2][i] != '':
+        if tabuleiro[0][i] == tabuleiro[1][i] == tabuleiro[2][i] != '':
             return tabuleiro[0][i]
     if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] != '':
         return tabuleiro[0][0]
@@ -89,89 +106,84 @@ def multiplayer():
     jogador_atual = 'X'
     while True:
         imprimir_tabuleiro()
-        linha = coordenada_linha()
-        coluna = coordenada_coluna()
+        jogada_usuario(jogador_atual)
         
-        if posicao_valida(linha, coluna):
-            jogar(jogador_atual, linha, coluna) #função de fazer a jogada
-            
-            vencedor = verificar_vencedor()
-            if vencedor:
-                imprimir_tabuleiro()
-                print(f'{vencedor} venceu!')
-                return vencedor
-            elif verificar_velha():
-                imprimir_tabuleiro()
-                print('Velha!')
-                return None
-            jogador_atual = 'O' if jogador_atual == 'X' else 'X' #if ternario para alternar entre os jogadores
-        else:
-            print('Posição inválida! Por favor, escolha outra.')
+        vencedor = verificar_vencedor()
+        if vencedor:
+            imprimir_tabuleiro()
+            print(f'{vencedor} venceu a rodada!')
+            return vencedor
+        elif verificar_velha():
+            imprimir_tabuleiro()
+            print('Velha!')
+            return None
+        jogador_atual = 'O' if jogador_atual == 'X' else 'X'  # alternar entre os jogadores
 
 #jogada da máquina, fácil
 def jogada_maquina_facil():
     while True:
-        linha = random.randint(0,2)
-        coluna = random.randint(0,2)
+        linha = random.randint(0, 2)
+        coluna = random.randint(0, 2)
         if posicao_valida(linha, coluna):
             jogar('O', linha, coluna)
             break
 
 #modo contra a máquina, fácil
 def maquina_facil():
-    jogador_atual = 'X' #o usuário sempre será o 'x', a máquina será o 'o'
+    jogador_atual = 'X'  # o usuário sempre será o 'x', a máquina será o 'o'
     while True:
         imprimir_tabuleiro()
 
         if jogador_atual == 'X':
-            linha = coordenada_linha()
-            coluna = coordenada_coluna()
+            jogada_usuario(jogador_atual)
+        else:
+            jogada_maquina_facil()
 
-            if posicao_valida(linha, coluna):
-                jogar(jogador_atual, linha, coluna)
-            else:
-                print('Posição inválida! Por favor, escolha outra.')
-
-            vencedor = verificar_vencedor()
-            if vencedor:
-                imprimir_tabuleiro()
-                print(f'{vencedor} venceu!')
-                return vencedor
-            elif verificar_velha():
-                imprimir_tabuleiro()
-                print('Velha!')
-                return None
-            jogador_atual = 'O' if jogador_atual == 'X' else 'X'
-
+        vencedor = verificar_vencedor()
+        if vencedor:
+            imprimir_tabuleiro()
+            print(f'{vencedor} venceu a rodada!')
+            return vencedor
+        elif verificar_velha():
+            imprimir_tabuleiro()
+            print('Velha!')
+            return None
+        jogador_atual = 'O' if jogador_atual == 'X' else 'X'
 
 #lógica principal do jogo
 def main():
     global pontuacao_jogador_x, pontuacao_jogador_o
-    
-    while pontuacao_jogador_x < 3 and pontuacao_jogador_o <3:
+
+    while True:
         opcao = menu()
-        vencedor = None
+        pontuacao_jogador_x = 0
+        pontuacao_jogador_o = 0
         
-        if opcao == '1':
-            vencedor = multiplayer()
-        elif opcao == '2':
-            vencedor = maquina_facil()
+        while pontuacao_jogador_x < 3 and pontuacao_jogador_o < 3:
+            vencedor = None
+
+            if opcao == '1':
+                vencedor = multiplayer()
+            elif opcao == '2':
+                vencedor = maquina_facil()
+            else:
+                print('Opção inválida!')
+                continue
+
+            if vencedor == 'X':
+                pontuacao_jogador_x += 1
+            elif vencedor == 'O':
+                pontuacao_jogador_o += 1
+
+            mostrar_pontuacao()
+            inicializar_tabuleiro()
+
+        if pontuacao_jogador_x == 3:
+            print(f'Jogador X venceu o jogo! ({pontuacao_jogador_x} - {pontuacao_jogador_o})')
         else:
-            print('Opção inválida!')
-            continue
-        
-        if vencedor == 'X':
-            pontuacao_jogador_x += 1
-        elif vencedor == 'O':
-            pontuacao_jogador_o += 1
-        
-        mostrar_pontuacao()
-        inicializar_tabuleiro()
-    
-    if pontuacao_jogador_x == 3:
-        print('Jogador X venceu o jogo!')
-    else:
-        print('Jogador O venceu o jogo!')
+            print(f'Jogador O venceu o jogo! ({pontuacao_jogador_o} - {pontuacao_jogador_x})')
 
 #rodar o jogo
 main()
+
+
